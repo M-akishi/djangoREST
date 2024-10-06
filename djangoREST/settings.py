59995d488e1afe9e748d347e3ed0e513f68bb328
M-akishi <camposmaximiliano12@gmail.com>
2024-10-06 20:23:57 +0000

    actualizar wsgi y settings

--- djangoREST/settings.py
+++ djangoREST/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-tqb%qgb5n5p+$hbrq_45turojy^8sj7^02_w3)_5eatrja(fyn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR,"static/")
--- djangoREST/wsgi.py
+++ djangoREST/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+
+sys.path.append('/var/www/html/djangoREST')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoREST.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
